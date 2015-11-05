e6f6f94480582504c4ff1293e9c5b00e8b450000
Jeffrey Ruder <ruderjt@gmail.com>
2015-11-05 11:18:31 -0800

    Implement numbers divisible by 5

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 var pingPongOutput = function(inputNumber) {
   if(inputNumber % 3 === 0) {
     return "ping";
+  } else if (inputNumber % 5 === 0) {
+    return "pong";
   } else {
     return inputNumber;
   }
