46cd5bae5deebdd700a1ca857dce5d22107bec7d
Jeffrey Ruder <ruderjt@gmail.com>
2015-11-05 13:35:36 -0800

    Add initial implementation of DOM manipulation

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
-var validInput = function(input) {
-  if (parseInt(input) > 0) {
+var validInput = function(userInput) {
+  if (parseInt(userInput) > 0) {
     return true;
   } else {
     return false;
@@ -17,3 +17,22 @@ var pingPongOutput = function(inputNumber) {
     return inputNumber;
   }
 };
+
+//Process user input and use jQuery to modify DOM
+
+$(document).ready(function() {
+  $("#go-button").click(function() {
+    $("#output").empty();
+    $fieldInput = $("#pingPongInputField").val();
+    if (!(validInput($fieldInput))) {
+      $("#output").append("<br><p>Please enter a positive integer to Ping Pong.</p>");
+    } else {
+      $pingPongNumber = parseInt($("#pingPongInputField").val());
+      $("#output").append("<br><ul>");
+      for (i = 1; i <= $pingPongNumber; i++) {
+        $("#output").append("<li>" + pingPongOutput(i) + "</li>");
+      };
+      $("#output").append("</ul>");
+    };
+  });
+});
