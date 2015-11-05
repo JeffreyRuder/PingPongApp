47fa0a8f7efd295c9752f0c37e7bed2c54d11803
Jeffrey Ruder <ruderjt@gmail.com>
2015-11-05 11:10:43 -0800

    Add spec for divisible by 3

--- spec/specs.js
+++ spec/specs.js
@@ -2,4 +2,8 @@ describe('pingPongOutput', function() {
   it("is the number for most input numbers", function() {
     expect(pingPongOutput(2)).to.equal(2);
   });
+
+  it("is 'ping' for numbers divisible by 3", function() {
+    expect(pingPongOutput(6)).to.equal("ping");
+  });
 });
